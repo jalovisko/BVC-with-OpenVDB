ede9383b913126b272b47e39b8f67e76c0d4962f
Nikita Letov <letovnn@gmail.com>
2019-12-18 22:13:21 -0500

    First commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  OpenVDB_test
+//
+//  Created by Nikita Letovon 2019-12-18.
+//  Copyright © 2019 Additive Design and Manufacturing Lab, McGill University. All rights reserved.
+//
+
+#include <openvdb/openvdb.h>
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
